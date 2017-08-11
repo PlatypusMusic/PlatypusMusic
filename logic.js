18ebf6e9e86d13a4d082c90d1c6a3a8771d2e5c4
MMMamich <MMMamich@gmail.com>
2017-08-11 10:25:16 -0700

    First Commit

---
+++ logic.js
@@ -0,0 +1,46 @@
+// var lastFmApiKey = "6a39f3465b8cc8531c8558573311b265";
+
+ function searchBandsInTown(artist) {
+
+    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=8ce559d2fd9f4e234a3ac172db2d0ef6&format=json";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+
+      // Printing the entire object to console
+      console.log(response);
+      console.log(response.artist.name);
+      console.log(response.artist.bio);
+      console.log(response.artist.image[0]);
+
+      // Constructing HTML containing the artist information
+      var artistName = $("<h1>").text(response.artist.name);
+      var artistURL = $("<a>").attr("href", response.artist.url).append(artistName);
+      var artistImage = $("<img>").attr("src", response.artist.image[3]["#text"]);
+      var Bio = $("<h2>").text("Bio: " + response.artist.bio.summary);
+        
+       for ( var i = 0; i < 4; i++) {
+        var similarArtists = $("<h2>").text(response.artist.similar.artist[i].name);
+        similarArtists.css("color", "white");
+      
+      $("#artist-div").append(artistURL, artistImage, Bio);
+      $("#similarArtist-div").append(similarArtists);
+      Bio.css("color", "white");
+      }
+      
+      
+    });
+  }
+
+  // Event handler for user clicking the select-artist button
+  $("#select-artist").on("click", function(event) {
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+    // Storing the artist name
+    var artist = $("#artist-input").val().trim();
+
+    // Running the searchBandsInTown function (passing in the artist as an argument)
+    searchBandsInTown(artist);
+  });
